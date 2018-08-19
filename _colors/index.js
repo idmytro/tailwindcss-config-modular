a1956e0370234e48fa3fce2e11047f55dd3be52e
Dmytro Cisar <dmytro.cisar@gmail.com>
2018-08-19 11:51:30 +0300

    add comments

--- _colors/index.js
+++ _colors/index.js
@@ -1,3 +1,18 @@
+// https://github.com/tailwindcss/tailwindcss/blob/master/defaultConfig.stub.js#L30
+
+/*
+|-----------------------------------------------------------------------------
+| Colors                                  https://tailwindcss.com/docs/colors
+|-----------------------------------------------------------------------------
+|
+| The color palette defined above is also assigned to the "colors" key of
+| your Tailwind config. This makes it easy to access them in your CSS
+| using Tailwind's config helper. For example:
+|
+| .error { color: config('colors.red') }
+|
+*/
+
 const colors = [
   {transparent: 'transparent'},
   require('./bw'),
