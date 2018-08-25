cb1c45bd1af062a07a605a7e3b6bb1c2bcc11600
Dmytro Cisar <dmytro.cisar@gmail.com>
2018-08-25 01:25:00 +0100

    wip

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 // https://github.com/tailwindcss/tailwindcss/blob/master/defaultConfig.stub.js
 
+const colors = require('./_colors');
+
 module.exports = {
-  colors: require('./_colors'),
+  colors,
   screens: require('./_screens'),
   fonts: require('./_fonts'),
   textSizes: require('./_textSizes'),
   fontWeights: require('./_fontWeights'),
-  leading: require('./_leading')
+  leading: require('./_leading'),
+  // tracking
+  textColors: colors,
+  backgroundColors: colors,
+  // backgroundSize
+  // borderWidths
+  // borderColors: global.Object.assign({ default: colors['grey-light'] }, colors),
+  // borderRadius
+  // width
+  // height
+  // minWidth
+  // minHeight
+  // maxWidth
+  // maxHeight
+  // padding
+  // margin
+  // negativeMargin
+  // shadows
+  // zIndex
+  // opacity
+  // svgFill
+  // svgStroke
 };
